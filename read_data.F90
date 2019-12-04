92757bba95c50c6c84e89bc7c57c5bcf521864c1
Kevin Lo <you@example.com>
2019-12-04 15:13:02 -0800

    updated read_data.F90

--- read_data.F90
+++ read_data.F90
@@ -1,17 +1,23 @@
 program read_data
         implicit none
-        real, allocatable, dimension (:,:) ::A
-        real, allocatable, dimension (:)::b
+        real, allocatable, dimension (:,:) :: A
+        real, allocatable, dimension (:):: b
         integer :: i,j, rank
+        !Open the dat file
+        open (2, file = "A_1.dat")
+        !Read the file then store into an array A and remember rank
+        read(2,*) A 
+        call printMatrix(A, 3,3)
+end program read_data
 
-        open (unit = 2, file = "output.txt")
-        read(2,*) rank
-        allocate (A(rank,rank))
-        close(2)
+subroutine printMatrix(array, n, m)
 
-        do i = 1,rank,1
-                read(3,*) A(i,:)
-                print *, 'A= ', A
-        enddo
+implicit none
+  real, intent(in) :: array(n,m)
+  integer, intent(in) :: n,m
+integer :: i
+  do i = 1,n
+     print*, array(i,:)
+  end do
+end subroutine printMatrix
 
-end program
